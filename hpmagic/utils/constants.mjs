07215b941f88cb9dee3199eca8e58a1847fbfe46
Veronika Aas <veronikaaas@live.no>
2025-09-06 11:56:38 +0200

    feat: add constants to fetch API

---
+++ hpmagic/utils/constants.mjs
@@ -0,0 +1,11 @@
+export const BASE_URL = 'https://api.potterdb.com';
+
+export const BOOKS_URL = `${BASE_URL}/v1/books/`;
+
+export const CHARACTERS_URL = `${BASE_URL}/v1/characters/`;
+
+export const MOVIES_URL = `${BASE_URL}/v1/movies/`;
+
+export const POTIONS_URL = `${BASE_URL}/v1/potions/`;
+
+export const SPELLS_URL = `${BASE_URL}/v1/spells/`;
