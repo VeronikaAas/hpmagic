b97407656c36d99e30a83fd79decdb1fd77bb835
Veronika Aas <veronikaaas@live.no>
2025-09-08 19:36:30 +0200

    feat: add elements to homepage

---
+++ hpmagic/src/pages/home/index.jsx
@@ -0,0 +1,19 @@
+import '../../index.css';
+import '../../components/header/header.jsx';
+
+function Home() {
+  return (
+    <div className="home-container">
+      <img
+        src="/Owl.png"
+        alt="An illustration of an owl resembling Hedvig from Harry Potter"
+        className="hedvig"
+        loading="lazy"
+      />
+      <h1>A magical world awaits.</h1>
+      <h2>Step into Harry Potter's story.</h2>
+    </div>
+  );
+}
+
+export default Home;
