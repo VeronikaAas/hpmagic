1fd88d6df8731501d5cddbd70b2e1d32e6ec5078
Veronika Aas <veronikaaas@live.no>
2025-09-08 22:06:42 +0200

    feat: fetch and display books

--- hpmagic/src/pages/books/index.jsx
+++ hpmagic/src/pages/books/index.jsx
@@ -1,5 +1,43 @@
+import { useEffect, useState } from 'react';
+import { BOOKS_URL } from '../../utils/constants';
+import { Link } from 'react-router-dom';
+
 function Books() {
-  return <div>Books Page</div>;
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(BOOKS_URL)
+      .then(res => res.json())
+      .then(data => {
+        setBooks(data.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Failed to fetch books:', error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading books</p>;
+
+  return (
+    <div>
+      <h1>Books</h1>
+      <ul>
+        {books.map(book => (
+          <li key={book.id}>
+            <Link
+              to={`/books/${book.id}`}
+              state={{ title: book.attributes.title }}
+            >
+              {book.attributes.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default Books;
