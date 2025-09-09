47689889346cb147c0ba0737232676b194ada27c
Veronika Aas <veronikaaas@live.no>
2025-09-09 22:19:36 +0200

    refactor: add wrapper divs for improved styling and layout control

--- hpmagic/src/pages/home/index.jsx
+++ hpmagic/src/pages/home/index.jsx
@@ -1,17 +1,22 @@
 import '../../index.css';
 import '../../components/header/header.jsx';
+import './home.css';
 
 function Home() {
   return (
     <div className="home-container">
-      <img
-        src="/Owl.png"
-        alt="An illustration of an owl resembling Hedvig from Harry Potter"
-        className="hedvig"
-        loading="lazy"
-      />
-      <h1>A magical world awaits</h1>
-      <h2>Step into Harry Potter's story</h2>
+      <div className="image-content">
+        <img
+          src="/Owl.png"
+          alt="An illustration of an owl resembling Hedvig from Harry Potter"
+          className="hedvig"
+          loading="lazy"
+        />
+        <div className="text-content">
+          <h1>A magical world awaits</h1>
+          <h2>Step into Harry Potter's story</h2>
+        </div>
+      </div>
     </div>
   );
 }
