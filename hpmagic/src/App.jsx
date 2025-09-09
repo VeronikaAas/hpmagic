fc6eb4d219408bcbfeb3b38fa7e584cbb7632bdd
Veronika Aas <veronikaaas@live.no>
2025-09-09 21:02:40 +0200

    feat: add move details page to router

--- hpmagic/src/App.jsx
+++ hpmagic/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/home';
 import Books from './pages/books';
 import BookDetails from './pages/bookDetails';
 import Movies from './pages/movies';
+import MoviesDetails from './pages/movieDetails';
 import Characters from './pages/characters';
 import Spells from './pages/spells';
 import Potions from './pages/potions';
@@ -19,6 +20,7 @@ function App() {
         <Route path="books" element={<Books />} />
         <Route path="books/:id" element={<BookDetails />} />
         <Route path="movies" element={<Movies />} />
+        <Route path="movies/:id" element={<MoviesDetails />} />
         <Route path="characters" element={<Characters />} />
         <Route path="spells" element={<Spells />} />
         <Route path="potions" element={<Potions />} />
