6eafbf25d970790e2448cd2a0f4025971a5b9977
Veronika Aas <veronikaaas@live.no>
2025-09-09 21:04:20 +0200

    feat: add movie details page

---
+++ hpmagic/src/pages/movieDetails/index.jsx
@@ -0,0 +1,69 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { MOVIES_URL } from '../../utils/constants';
+
+function MovieDetails() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchMovie() {
+      try {
+        setLoading(true);
+        const movieRes = await fetch(`${MOVIES_URL}${id}`);
+        const movieData = await movieRes.json();
+        setMovie(movieData.data);
+      } catch (error) {
+        console.error('Error fetching:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchMovie();
+  }, [id]);
+
+  if (loading) return <p>Loading information about the movie...</p>;
+  if (!movie) return <p>Didn't find movie.</p>;
+
+  const attributes = movie.attributes;
+
+  return (
+    <div className="movies-container">
+      <h1>{attributes.title}</h1>
+
+      <p>
+        <strong>Release date:</strong> {attributes.release_date || 'Unknown'}
+      </p>
+
+      <p>
+        <strong>Running time:</strong> {attributes.running_time || 'Unknown'}
+      </p>
+
+      <p>
+        <strong>Rating:</strong> {attributes.rating || 'Unknown'}
+      </p>
+
+      <p>
+        <strong>Budget:</strong> {attributes.budget || 'Unknown'}
+      </p>
+
+      <p>
+        <strong>Screenwriters:</strong>{' '}
+        {attributes.screenwriters?.join(', ') || 'Unknown'}
+      </p>
+
+      <p>
+        <strong>Producers:</strong>{' '}
+        {attributes.producers?.join(', ') || 'Unknown'}
+      </p>
+
+      <p>
+        <strong>Description:</strong>{' '}
+        {attributes.summary || 'No description available.'}
+      </p>
+    </div>
+  );
+}
+
+export default MovieDetails;
