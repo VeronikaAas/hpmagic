b729d78da1ce09d8f90cbd7c46a17c5b5606bdc9
Veronika Aas <veronikaaas@live.no>
2025-09-09 22:25:54 +0200

    refactor: simplify Card component structure and update heading levels

--- hpmagic/src/components/card/elementCard.jsx
+++ hpmagic/src/components/card/elementCard.jsx
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 
 function Card({ title, to, onClick }) {
-  const content = (
+  if (to) {
+    return (
+      <Link to={to} className="card">
+        <h3 className="card-title">{title}</h3>
+      </Link>
+    );
+  }
+
+  return (
     <div className="card" onClick={onClick}>
-      <h2 className="card-title">{title}</h2>
+      <h3 className="card-title">{title}</h3>
     </div>
   );
-
-  return to ? <Link to={to}>{content}</Link> : content;
 }
-
 export default Card;
